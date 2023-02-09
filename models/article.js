b539caa8b2e75527870069f1c0fd0fd8fe472c6b
FedericoAntunes <fedeantunes1234@gmail.com>
2023-02-09 14:54:17 -0300

    Created Article model

---
+++ models/article.js
@@ -0,0 +1,46 @@
+const { Sequelize, Model, Datatypes } = require("sequelize");
+
+class Article extends Model {}
+
+Article.init(
+  {
+    id: {
+      primearyKey: true,
+      autoIncrement: true,
+      type: Datatypes.BIGINT,
+    },
+    title: {
+      allowNull: false,
+      type: Datatypes.STRING(100),
+    },
+    content: {
+      allowNull: false,
+      type: Datatypes.TEXT,
+    },
+    image: {
+      allowNull: false,
+      type: Datatypes.STRING(1500),
+    },
+    date: {
+      allowNull: false,
+      type: Datatypes.DATE,
+    },
+    author_name: {
+      allowNull: false,
+      type: Datatypes.STRING(100),
+    },
+    author_last_name: {
+      allowNull: false,
+      type: Datatypes.STRING(100),
+    },
+    author_email: {
+      allowNull: false,
+      type: Datatypes.STRING(100),
+    },
+    comments: {
+      allowNull: false,
+      type: Datatypes.STRING(1000),
+    },
+  },
+  { sequelize, modelName: "article" }
+);
