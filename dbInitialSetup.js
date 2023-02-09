8a26643cca8151f90c34fa62adde2069fe17a592
mateo-mendez <mateomendez@protonmail.com>
2023-02-09 15:31:19 -0300

    dbInitialSetup fix

--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -2,5 +2,5 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("ha_ejercicio_21", "root", "root1234", {
     host: "127.0.0.1",
-    dialect "mysql",
+    dialect: "mysql",
 });
