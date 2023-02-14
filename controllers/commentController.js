81fa612d9ba84c387a5ae00cec5689166a02d78c
joaquin1990 <joaquingarese@gmail.com>
2023-02-14 12:29:09 -0300

    Limpiando los controllers comment y pages

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,31 +1,6 @@
-/**
- * Este archivo se puede usar como referencia para crear el controlador de
- * cualquier entidad del sistema.
- *
- * Por ejemplo, si se necesita crear un controlador para la entidad `Student`,
- * se sugiere hacer Copy & Paste de este archivo y nombrarlo como
- * `studentController.js`.
- *
- * No es necesario renombrar los métodos. A priori, la idea es que todos los
- * controladores tengan estos 7 métodos: index, show, create, store, edit,
- * update y destroy.
- *
- * Nota: en el caso de estar creando una API, los métodos `create` y `edit`
- * no serían necesarios ya que los mismos se usan en proyecto con server-side
- * rendering para mostrar los formularios de crear y editar, respectivamente.
- *
- * Por lo tanto, al crear una API, los únicos métodos que serían necesarios
- * son: index, show, store, update y destroy.
- */
 const { Comment } = require("../models");
 
-// Display a listing of the resource.
-async function index(req, res) {}
-
-// Display the specified resource.
-async function show(req, res) {}
-
-// Show the form for creating a new resource
+// Create a new comment:
 async function create(req, res) {
   try {
     const content = req.body.content;
@@ -42,10 +17,7 @@ async function create(req, res) {
   }
 }
 
-// Store a newly created resource in storage.
-async function store(req, res) {}
-
-// Show the form for editing the specified resource.
+// Edit a comment
 async function edit(req, res) {
   const content = req.body.content;
   const commentToEdit = await Comment.findByPk(req.params.id);
@@ -57,10 +29,7 @@ async function edit(req, res) {
   res.redirect("/panel/comentarios");
 }
 
-// Update the specified resource in storage.
-async function update(req, res) {}
-
-// Remove the specified resource from storage.
+// Destroy a comment:
 async function destroy(req, res) {
   await Comment.destroy({
     where: { id: req.params.id },
@@ -69,15 +38,8 @@ async function destroy(req, res) {
   res.redirect("/panel/comentarios");
 }
 
-// Otros handlers...
-// ...
-
 module.exports = {
-  index,
-  show,
   create,
-  store,
   edit,
-  update,
   destroy,
 };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const commentController = require("../controllers/commentController");
 
 // Rutas relacionadas a los comentarios:
-// ...
+
 router.post("/crear/:id", commentController.create);
 router.post("/:id/editar", commentController.edit);
 router.get("/destroy/:id", commentController.destroy);
